42056ffe57e461d3bc161cfe71461e73374cb2df
Shy Ruparel <Shyamalruparel1991@gmail.com>
2019-05-07 15:44:05 -0400

    Added initial go at styling

--- app.py
+++ app.py
@@ -1,24 +1,18 @@
-# To run
-# $ export FLASK_APP=app.py
-# $ python -m flask run
-
 from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
 
 import contentful
 from rich_text_renderer import RichTextRenderer
 import os
 
-SPACE_ID = os.environ.get('SPACE_ID')
-DELIVERY_API_KEY = os.environ.get('DELIVERY_API_KEY')
-API_URL = os.environ.get('API_URL')
+SPACE_ID = os.environ.get("SPACE_ID")
+DELIVERY_API_KEY = os.environ.get("DELIVERY_API_KEY")
+API_URL = os.environ.get("API_URL")
 
 client = contentful.Client(SPACE_ID, DELIVERY_API_KEY, API_URL)
 
 renderer = RichTextRenderer()
 
 app = Flask(__name__)
-Bootstrap(app)
 
 
 @app.route("/")
@@ -30,6 +24,7 @@ def home_page():
         renderer=renderer,
         title=entry.page_title,
         page_components=entry.page_component,
+        client=client,
     )
 
 
