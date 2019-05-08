4c7e9c2a3c48304b012581205f96bf33e39907ec
Shy Ruparel <Shyamalruparel1991@gmail.com>
2019-05-08 14:05:45 -0400

    Changed map to static until clickeD

---
+++ static/js/main.js
@@ -0,0 +1,6 @@
+const swap_map = () => {
+    if (document.getElementById('map').style.display === 'none') {
+        document.getElementById('map').style.display = 'block'
+        document.getElementById('static_map').style.display = 'none'
+    }
+}
