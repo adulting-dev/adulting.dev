d2c0f93d1269137c1f6f6790f0af0a8293c27a6e
Shy Ruparel <Shy@users.noreply.github.com>
2023-03-15 15:19:56 -0400

    responsive iFrame

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,11 @@
 const swap_map = () => {
-    if (document.getElementById('map').style.display === 'none') {
-        document.getElementById('map').style.display = 'block'
-        document.getElementById('static_map').style.display = 'none'
+    if (document.getElementById("map").style.display === "none") {
+        let static_map = document.querySelector("#static_map");
+        let map = document.querySelector("#map");
+        mapChild = document.getElementById("mapChild");
+        mapChild.width = static_map.width;
+        mapChild.height = static_map.height;
+        map.style.display = "block";
+        map.style.display = "none";
     }
-}
+};
